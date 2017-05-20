d1029aa50ac6c548e514db8f3a5e951321c2b9a3
aitthi arsa <meanstack20@gmail.com>
2017-05-21 01:29:31 +0700

    add ng build

--- src/main.ts
+++ src/main.ts
@@ -2,8 +2,9 @@ import { enableProdMode } from '@angular/core';
 import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 
 import { AppModule } from './app/app.module';
+import { environment } from './environments/environment';
 
-if (process.env.ENV === 'production') {
+if (process.env.ENV === 'production' || environment.production) {
   enableProdMode();
 }
 
